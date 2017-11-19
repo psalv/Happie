aae93a05c1a629c3bf20d9bc1f0fef68c96f7f03
Kiran <kirantatla5@gmail.com>
2017-11-19 01:16:42 -0500

    color

--- Happie/mainScreen.swift
+++ Happie/mainScreen.swift
@@ -12,8 +12,8 @@ import UIKit
 class mainScreen: UIViewController {
     
     override func viewDidLoad() {
-        self.view.backgroundColor = UIColor.gray
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.init(red: 33/255, green: 150/255, blue: 243/255, alpha:1) //blue 500
         startUp()
     }
     
@@ -24,6 +24,7 @@ class mainScreen: UIViewController {
     private var labels=[UILabel]()
 
     @IBOutlet weak var headerText: UILabel!
+    @IBOutlet weak var footerText: UILabel!
     
     @IBOutlet weak var random: UILabel!
     @IBOutlet weak var gratitude: UILabel!
@@ -31,23 +32,32 @@ class mainScreen: UIViewController {
     @IBOutlet weak var walk: UILabel!
     @IBOutlet weak var journal: UILabel!
     
+    
+    
+    
     func startUp(){
         labels.append(random)
         labels.append(gratitude)
         labels.append(medidate)
         labels.append(walk)
         labels.append(journal)
-        headerText.font = headerText.font.withSize(25.0)
+        
+//        headerText.backgroundColor=UIColor.init(red: 26/255, green: 35/255, blue: 126/255, alpha:1) //indigo 900
+//        footerText.backgroundColor=UIColor.init(red: 26/255, green: 35/255, blue: 126/255, alpha:1) //indigo 900
+        headerText.backgroundColor=UIColor.black
+        footerText.backgroundColor=UIColor.black
+        headerText.textColor=UIColor.white
+        headerText.font=headerText.font.withSize(20.0)
+        
         
         for label in labels{
             label.font = label.font.withSize(35.0)
             label.backgroundColor=UIColor.white
-            label.textColor=UIColor.gray
+//            label.textColor=UIColor.init(red: 13/255, green: 71/255, blue: 161/255, alpha:1) //blue 900
+            label.textColor=UIColor.black
         }
         
 
     }
-    
-    
     
 }
