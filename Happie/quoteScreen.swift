d9373141309cc739c63ceaf774684b1493f23bc3
psalv <psalvato@uwo.ca>
2017-11-19 02:32:54 -0500

    added quote screen

---
+++ Happie/quoteScreen.swift
@@ -0,0 +1,27 @@
+//
+//  quoteScreen.swift
+//  Happie
+//
+//  Created by Paul Salvatore on 2017-11-19.
+//  Copyright © 2017 GP Productions. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class quoteScreen: UIViewController {
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
